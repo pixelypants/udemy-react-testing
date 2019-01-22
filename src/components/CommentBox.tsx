0a5e3d7b20749eff9b308e2f7acb46979f14feef
pixelypants <hudson.graham@gmail.com>
2019-01-22 12:36:57 +1000

    Completed CommentBox testing

--- src/components/CommentBox.tsx
+++ src/components/CommentBox.tsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { defaultCoreCipherList } from 'constants';
 
-export default () => {
-    return <div>Comment Box</div>
+class CommentBox extends Component {
+    state = { comment: '' };
+
+    handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        this.setState({ comment: event.target.value })
+    }
+
+    handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        // TODO: Call an action creator
+        // and save the comment using redux
+
+        this.setState({ comment: '' })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h4>Add a Comment</h4>
+                <textarea onChange={this.handleChange} value={this.state.comment} />
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+        )
+    }
 }
+
+export default CommentBox;
---
+++ src/components/__tests__/CommentBox.test.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { mount, ReactWrapper } from "enzyme";
+import CommentBox from "../CommentBox";
+
+let wrapped: ReactWrapper;
+
+beforeEach(() => {
+    wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+    wrapped.unmount();
+})
+
+it('shows a text area and a button', () => {
+
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
+});
+
+describe('the textarea', () => {
+
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment' }
+        })
+        wrapped.update();
+    });
+
+    it('has a textarea thay users cam type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    });
+
+    it('should clear the textarea when the form is submitted', () => {
+        wrapped.find('form').simulate('submit')
+        wrapped.update();
+
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+});
